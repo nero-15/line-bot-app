624d2deb309ebc1d6ff93f0c7c55b49f6d3abd51
nero-15 <nero15.dev@gmail.com>
2021-08-22 22:04:19 +0900

    send message

--- line-bot-app/go.mod
+++ line-bot-app/go.mod
@@ -4,5 +4,5 @@ go 1.14
 
 require (
 	github.com/labstack/echo/v4 v4.5.0
-	github.com/line/line-bot-sdk-go/v7 v7.10.0 // indirect
+	github.com/line/line-bot-sdk-go/v7 v7.10.0
 )
--- line-bot-app/main.go
+++ line-bot-app/main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
-    "net/http"
-	echo "github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+    "log"
+    //"net/http"
+	//echo "github.com/labstack/echo/v4"
+	//"github.com/labstack/echo/v4/middleware"
+    "github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 func main() {
-	e := echo.New()
+    // TODO: チャネルシークレットとチャネルトークンを追加
+    bot, err := linebot.New("<channel secret>", "<channel access token>")
+    if err != nil {
+        log.Fatal(err)
+    }
+    message := linebot.NewTextMessage("hello, world")
+    if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+        log.Fatal(err)
+    }
 
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: `"time":"${time_rfc3339}","remote_ip":"${remote_ip}","host":"${host}",` +
-			`"method":"${method}","uri":"${uri}","status":${status},"error":"${error}"` + "\n",
-	}))
-	e.Use(middleware.Recover())
 
-	e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "hello world")
-	})
 
-	e.Logger.Fatal(e.Start(":1234"))
+	// e := echo.New()
+    //
+	// e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	// 	Format: `"time":"${time_rfc3339}","remote_ip":"${remote_ip}","host":"${host}",` +
+	// 		`"method":"${method}","uri":"${uri}","status":${status},"error":"${error}"` + "\n",
+	// }))
+	// e.Use(middleware.Recover())
+    //
+	// e.GET("/", func(c echo.Context) error {
+    //     return c.String(http.StatusOK, "hello world")
+	// })
+    //
+	// e.Logger.Fatal(e.Start(":1234"))
 }
