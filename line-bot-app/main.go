7dfb0d6185f377ff104ae67f83cda225383afd68
nero-15 <nero15.dev@gmail.com>
2021-09-04 23:08:44 +0900

    fix

--- line-bot-app/main.go
+++ line-bot-app/main.go
@@ -2,36 +2,24 @@ package main
 
 import (
     "log"
-    //"net/http"
-	//echo "github.com/labstack/echo/v4"
-	//"github.com/labstack/echo/v4/middleware"
+    "os"
+
     "github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 func main() {
-    // TODO: チャネルシークレットとチャネルトークンを追加
-    bot, err := linebot.New("<channel secret>", "<channel access token>")
+    bot, err := linebot.New(
+        os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+        os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+    )
+
     if err != nil {
         log.Fatal(err)
     }
+
     message := linebot.NewTextMessage("hello, world")
+
     if _, err := bot.BroadcastMessage(message).Do(); err != nil {
         log.Fatal(err)
     }
-
-
-
-	// e := echo.New()
-    //
-	// e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-	// 	Format: `"time":"${time_rfc3339}","remote_ip":"${remote_ip}","host":"${host}",` +
-	// 		`"method":"${method}","uri":"${uri}","status":${status},"error":"${error}"` + "\n",
-	// }))
-	// e.Use(middleware.Recover())
-    //
-	// e.GET("/", func(c echo.Context) error {
-    //     return c.String(http.StatusOK, "hello world")
-	// })
-    //
-	// e.Logger.Fatal(e.Start(":1234"))
 }
